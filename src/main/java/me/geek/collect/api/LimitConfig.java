e8b8bbbadf997b3bfff3c7bcff1667b9de936a96
老廖 <1349517404@qq.com>
2023-10-04 01:01:24 +0800

    Initial commit

---
+++ src/main/java/me/geek/collect/api/LimitConfig.java
@@ -0,0 +1,22 @@
+package me.geek.collect.api;
+
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * GeekCollectLimit
+ * me.geek.collect.api
+ *
+ * @author 老廖
+ * @since 2023/10/3 11:13
+ */
+public interface LimitConfig {
+
+    @NotNull
+    Material getMaterial();
+
+    @NotNull
+    String getDisplayName();
+
+    int getAmount();
+}
---
+++ src/main/java/me/geek/collect/api/PermConfig.java
@@ -0,0 +1,28 @@
+package me.geek.collect.api;
+
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+
+/**
+ * GeekCollectLimit
+ * me.geek.collect.api
+ *
+ * @author 老廖
+ * @since 2023/10/3 11:16
+ */
+public interface PermConfig extends Comparable<PermConfig> {
+
+    int getPriority();
+
+    @NotNull
+    String getPermission();
+
+    @Nullable
+    LimitConfig getConfig(Material type);
+
+    @NotNull
+    Set<Material> getKeys();
+}
